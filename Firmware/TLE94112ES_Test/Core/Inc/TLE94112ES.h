88547832c26768127ea76f65b65760afd893b028
ruumoo <remo.kagerer@gmail.com>
2021-09-21 18:01:23 +0200

    created

---
+++ Firmware/TLE94112ES_Test/Core/Inc/TLE94112ES.h
@@ -0,0 +1,13 @@
+/*
+ * TLE94112ES.h
+ *
+ *  Created on: 21.09.2021
+ *      Author: Remo
+ */
+
+#ifndef INC_TLE94112ES_H_
+#define INC_TLE94112ES_H_
+
+
+
+#endif /* INC_TLE94112ES_H_ */
---
+++ Firmware/TLE94112ES_Test/Core/Src/TLE94112ES.c
@@ -0,0 +1,8 @@
+/*
+ * TLE94112ES.c
+ *
+ *  Created on: 21.09.2021
+ *      Author: Remo
+ */
+
+
