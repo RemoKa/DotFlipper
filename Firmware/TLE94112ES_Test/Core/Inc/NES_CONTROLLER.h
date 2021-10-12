5c57ccdc5f9bc29ec619c540b0ee355be3c146ae
Remo Kagerer <remo.kagerer@hm.edu>
2021-10-12 12:15:02 +0200

    defined enum for button bitmasks

--- Firmware/TLE94112ES_Test/Core/Inc/NES_CONTROLLER.h
+++ Firmware/TLE94112ES_Test/Core/Inc/NES_CONTROLLER.h
@@ -12,8 +12,16 @@
 extern "C" {
 #endif
 
-
-
+typedef enum NES_Buttons{
+	NES_A 		= 0b00000001,
+	NES_B		= 0b00000010,
+	NES_SELECT	= 0b00000100,
+	NES_START 	= 0b00001000,
+	NES_UP		= 0b00010000,
+	NES_DOWN	= 0b00100000,
+	NES_LEFT	= 0b01000000,
+	NES_RIGHT	= 0b10000000,
+}NES_Buttons;
 
 
 #ifdef __cplusplus
