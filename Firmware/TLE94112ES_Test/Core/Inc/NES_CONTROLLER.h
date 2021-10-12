ecbaf4cf514911c9c658d4de17939decefabb6ac
Remo Kagerer <remo.kagerer@hm.edu>
2021-10-12 12:03:22 +0200

    initial commit

---
+++ Firmware/TLE94112ES_Test/Core/Inc/NES_CONTROLLER.h
@@ -0,0 +1,22 @@
+/*
+ * NES_CONTROLLER.h
+ *
+ *  Created on: 12.10.2021
+ *      Author: Remo Kagerer
+ */
+
+#ifndef INC_NES_CONTROLLER_H_
+#define INC_NES_CONTROLLER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* INC_NES_CONTROLLER_H_ */
---
+++ Firmware/TLE94112ES_Test/Core/Src/NES_CONTROLLER.c
@@ -0,0 +1,8 @@
+/*
+ * NES_CONTROLLER.c
+ *
+ *  Created on: 12.10.2021
+ *      Author: Remo
+ */
+
+#include "NES_CONTROLLER.h"
