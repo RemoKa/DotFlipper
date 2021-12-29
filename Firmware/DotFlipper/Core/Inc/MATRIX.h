1004ae7c024b231510c9bcfdcd3692c9a1ffa1e9
ruumoo <remo.kagerer@gmail.com>
2021-12-29 16:48:13 +0100

    Create MATRIX.h

---
+++ Firmware/DotFlipper/Core/Inc/MATRIX.h
@@ -0,0 +1,18 @@
+/*
+ * MATRIX.h
+ *
+ *  Created on: 29.12.2021
+ *      Author: Remo Kagerer
+ */
+
+#ifndef INC_MATRIX_H_
+#define INC_MATRIX_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* INC_MATRIX_H_ */
