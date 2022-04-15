ac16eb967974a44cca355b426bb11e67f1bd3ff1
ruumoo <remo.kagerer@gmail.com>
2022-04-15 23:26:04 +0200

    initial commit

--- Firmware/DotFlipper2/Core/Inc/DISPLAY.h
+++ Firmware/DotFlipper2/Core/Inc/DISPLAY.h
@@ -26,7 +26,7 @@ typedef struct Display{
 
 void Display_setPixel(Display *Display, uint16_t x, uint16_t y, uint8_t color);
 
-void Display_swap();
+void Display_swap(Display *Display);
 
 void Display_draw();
 
---
+++ Firmware/DotFlipper2/Core/Src/DISPLAY.c
@@ -0,0 +1,18 @@
+/*
+ * DISPLAY.c
+ *
+ *  Created on: 15.04.2022
+ *      Author: Remo
+ */
+
+#include "DISPLAY.h"
+
+void Display_setPixel(Display *Display, uint16_t x, uint16_t y, uint8_t color);
+
+void Display_swap(Display *Display){
+
+
+}
+
+void Display_draw();
+
