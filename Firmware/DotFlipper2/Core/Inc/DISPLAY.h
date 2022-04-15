9b6feea416177e436e853186447cfb26e56d329b
ruumoo <remo.kagerer@gmail.com>
2022-04-15 23:46:56 +0200

    renamed variable

--- Firmware/DotFlipper2/Core/Inc/DISPLAY.h
+++ Firmware/DotFlipper2/Core/Inc/DISPLAY.h
@@ -24,9 +24,9 @@ typedef struct Display{
 }Display;
 
 
-void Display_setPixel(Display *Display, uint16_t x, uint16_t y, uint8_t color);
+void Display_setPixel(Display *display, uint16_t x, uint16_t y, uint8_t color);
 
-void Display_swap(Display *Display);
+void Display_swap(Display *display);
 
 void Display_draw();
 
