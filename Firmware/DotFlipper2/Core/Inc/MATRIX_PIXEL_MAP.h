7c0ecd2f67ae3338d0be3a354889b2de415f79b7
ruumoo <remo.kagerer@gmail.com>
2022-05-22 22:17:25 +0200

    renamed and added include guards

---
+++ Firmware/DotFlipper2/Core/Inc/MATRIX_PIXEL_MAP.h
@@ -0,0 +1,19 @@
+/*
+ * MATRIX_PIXEL_MAP.h
+ *
+ *  Created on: 22.05.2022
+ *      Author: Remo
+ */
+
+#ifndef INC_MATRIX_PIXEL_MAP_H_
+#define INC_MATRIX_PIXEL_MAP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* INC_MATRIX_PIXEL_MAP_H_ */
