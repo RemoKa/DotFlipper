0fc45bf3bdee7fd53127ff0bb76b0a2e65460780
ruumoo <remo.kagerer@gmail.com>
2022-06-04 09:20:43 +0200

    basic implementation of  flipping single pixel function

--- Firmware/DotFlipper2/Core/Src/MATRIX.c
+++ Firmware/DotFlipper2/Core/Src/MATRIX.c
@@ -8,3 +8,95 @@
 
 #include "MATRIX.h"
 
+void setPixel(uint8_t x, uint8_t y, uint8_t state, uint8_t *TXBuf, uint8_t *RXBuf, uint8_t daisyChainLength, SPI_HandleTypeDef hspi){
+
+	Message messages[daisyChainLength];
+	clearMessages(messages, daisyChainLength);
+
+	//lookup row and column info of pixel
+	Column *c = &ColumnMap[x];
+	Row *r = &RowMap[y];
+
+	//cache messages in message index that concern this pixel
+	Message* mRH = &messages[r.high.chainIndex];
+	Message* mRL = &messages[r.low.chainIndex];
+	Message* mC = &messages[c.pin.chainIndex];
+
+	if(state){
+
+
+
+		clearMessages(messages, daisyChainLength);
+		//set row high Pin to high
+
+		setPin(mRH, r.high, 1);
+		setPin(mC, c.pin, 0);
+
+		TLE94112ES_ConstructTXBuffer(TXBuf, daisyChainLength, messages);
+
+		HAL_GPIO_WritePin(SPI2_CS_GPIO_Port, SPI2_CS_Pin, 0);
+		HAL_SPI_TransmitReceive(&hspi, TXBuf, RXBuf, daisyChainLength * 2, HAL_MAX_DELAY);
+		HAL_GPIO_WritePin(SPI2_CS_GPIO_Port, SPI2_CS_Pin, 1);
+	  	TLE94112ES_DeconstructRXBuffer(RXBuf, daisyChainLength, messages);
+		//send buffer
+
+		HAL_Delay(PULSE_LENGHT);
+
+		clearMessages(messages, daisyChainLength);
+
+		//set Pins back to HighZ
+
+		setPin(mRH, r.high, -1);
+		setPin(mC, c.pin, -1);
+
+		TLE94112ES_ConstructTXBuffer(TXBuf, daisyChainLength, messages);
+
+		HAL_GPIO_WritePin(SPI2_CS_GPIO_Port, SPI2_CS_Pin, 0);
+		HAL_SPI_TransmitReceive(&hspi, TXBuf, RXBuf, daisyChainLength * 2, HAL_MAX_DELAY);
+		HAL_GPIO_WritePin(SPI2_CS_GPIO_Port, SPI2_CS_Pin, 1);
+	  	TLE94112ES_DeconstructRXBuffer(RXBuf, daisyChainLength, messages);
+		//send buffer
+
+	}else{
+
+	}
+
+	//set row high or low pin
+
+	//turn column pin opposite on
+	//delay
+	//set column pin highZ
+
+	//set row pin highZ
+
+}
+
+void clearMessages(Message *messages, uint8_t lenght){
+	for(int i = 0; i < lenght; i++){
+			messages[i] = emptyMessage;
+	}
+}
+
+void setPin(Message* m, Pin p, int8_t state){
+	m->RegisterAdress = HbActCtrlLookup[p.hBridgeIndex];
+	switch (state){
+	case -1: //HighZ
+		m->Data &= !HbEnLookup[p.hBridgeIndex][0];
+		m->Data &= !HbEnLookup[p.hBridgeIndex][1];
+		break;
+
+	case 0:	//Low
+		m->Data |= HbEnLookup[p.hBridgeIndex][0];
+		m->Data &= !HbEnLookup[p.hBridgeIndex][1];
+		break;
+
+	case 1: //High
+		m->Data &= !HbEnLookup[p.hBridgeIndex][0];
+		m->Data |= HbEnLookup[p.hBridgeIndex][1];
+		break;
+	default:
+		break;
+	}
+	m->WriteClear = WRITE;
+}
+
